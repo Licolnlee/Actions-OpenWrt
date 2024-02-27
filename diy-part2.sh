247e5bbe51dac06b1cfec574bd67e67e2bca28f5
Licolnlee <me@licolnlee.icu>
2024-02-27 19:43:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,11 +10,11 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_generate
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
-git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
-git clone https://github.com/rufengsuixing/luci-app-autoipsetadder package/luci-app-autoipsetadder
+#sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_generate
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+#git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+#git clone https://github.com/rufengsuixing/luci-app-autoipsetadder package/luci-app-autoipsetadder
 #git clone https://github.com/pymumu/openwrt-smartdns package/openwrt-smartdns
 #git clone https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
-git clone https://github.com/Licolnlee/Feed-Licolnlee package/Feed-Licolnlee
+#git clone https://github.com/Licolnlee/Feed-Licolnlee package/Feed-Licolnlee
 
